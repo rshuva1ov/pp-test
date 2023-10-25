db771909372dec8885948658a11b9dc5d4682b1f
unknown <shuvalov.rem@mail.ru>
2023-10-25 09:43:16 +0800

    finaly

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -20,7 +20,7 @@ module.exports = {
   mode: NODE_ENV ? NODE_ENV : "development",
   entry: [
     path.resolve(__dirname, "../src/client/index.jsx"),
-    "webpack-hot-middleware/client?path=//localhost:3001/static/__webpack_hmr",
+    // "webpack-hot-middleware/client?path=//localhost:3001/static/__webpack_hmr",
   ],
   output: {
     path: path.resolve(__dirname, "../dist/client"),
